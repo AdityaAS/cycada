9fea8a36b36f4263dc906acfdcbb858d60ce1797
rudra-wadhwani <sohan.rudra@wadhwaniai.org>
2019-05-21 12:40:31 +0000

    fixed minor errors

--- cycada/data/blender.py
+++ cycada/data/blender.py
@@ -1,3 +1,4 @@
+import sys
 import os.path
 import numpy as np
 import scipy.io
@@ -5,17 +6,17 @@ import torch
 import torch.utils.data as data
 from glob import glob
 from PIL import Image
+#sys.path.append('..')
+from data.data_loader import register_data_params, register_dataset_obj
+from data.data_loader import DatasetParams
 
-from .data_loader import register_data_params, register_dataset_obj
-from .data_loader import DatasetParams
-
-@register_dataset_params('blender')
+@register_data_params('blender')
 class BlenderParams(DatasetParams):
-	num_channels = 3
-	image_size = 224
-	mean = 0.5
-	num_cls = 2
-	target_transform = None
+    num_channels = 3
+    image_size = 224
+    mean = 0.5
+    num_cls = 2
+    target_transform = None
 
 @register_dataset_obj('blender')
 class Blender(data.Dataset):
@@ -28,25 +29,25 @@ class Blender(data.Dataset):
         self.ids = self.collect_ids()
         self.transform = transform
         self.target_transform = target_transform
-        self.img_path = os.path.join(self.root, self.split, 'paired', 'images')
-        self.seg_path = os.path.join(self.root, self.split, 'paired', 'segmasks')
+        self.im_path = os.path.join(self.root, self.split, 'paired', 'images')
+        self.l_path = os.path.join(self.root, self.split, 'paired', 'segmasks')
         self.num_cls = num_cls
 
     
     def collect_ids(self):
-        pathname = os.path.join(self.img_path, '*')
+        pathname = os.path.join(self.root, self.split, 'paired', 'images/*')
         list_file = glob(pathname)
         id_len = len(list_file)
         ids = ['{:05d}'.format(i) for i in range(id_len)]
         return ids
 
     def img_path(self, id):
-        filename = '{:05d}.png'.format(id)
-        return os.path.join(self.img_path, filename)
+        filename = "0" + id + ".png"
+        return os.path.join(self.im_path, filename)
 
     def label_path(self, id):
-        filename = '{:05d}.png'.format(id)
-        return os.path.join(self.seg_path, filename)
+        filename = id + ".png"
+        return os.path.join(self.l_path, filename)
 
     def __getitem__(self, index):
         id = self.ids[index]
--- cycada/data/opendr.py
+++ cycada/data/opendr.py
@@ -6,10 +6,10 @@ import torch.utils.data as data
 from glob import glob
 from PIL import Image
 
-from .data_loader import register_data_params, register_dataset_obj
-from .data_loader import DatasetParams
+from data.data_loader import register_data_params, register_dataset_obj
+from data.data_loader import DatasetParams
 
-@register_dataset_params('opendr')
+@register_data_params('opendr')
 class OpenDRParams(DatasetParams):
     num_channels = 3
     image_size = 480
@@ -28,25 +28,25 @@ class OpenDR(data.Dataset):
         self.ids = self.collect_ids()
         self.transform = transform
         self.target_transform = target_transform
-        self.img_path = os.path.join(self.root, self.split, 'paired', 'images')
-        self.seg_path = os.path.join(self.root, self.split, 'paired', 'segmasks')
+        self.im_path = os.path.join(self.root, self.split, 'paired', 'images')
+        self.l_path = os.path.join(self.root, self.split, 'paired', 'segmasks')
         self.num_cls = num_cls
 
     
     def collect_ids(self):
-        pathname = os.path.join(self.img_path, '*')
+        pathname = os.path.join(self.root, self.split, 'paired', 'images/*')
         list_file = glob(pathname)
         id_len = len(list_file)
         ids = ['{:05d}'.format(i) for i in range(id_len)]
         return ids
 
     def img_path(self, id):
-        filename = '{:05d}.png'.format(id)
-        return os.path.join(self.img_path, filename)
+        filename = id + ".jpg"
+        return os.path.join(self.im_path, filename)
 
     def label_path(self, id):
-        filename = '{:05d}.png'.format(id)
-        return os.path.join(self.seg_path, filename)
+        filename = id + ".png"
+        return os.path.join(self.l_path, filename)
 
     def __getitem__(self, index):
         id = self.ids[index]
