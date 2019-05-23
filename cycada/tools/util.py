0d2a650cb4044bb357ed86e5a4790e4a5fefbfd0
Aditya Sarma <aditya@wadhwaniai.org>
2019-05-23 21:51:57 +0530

    Add first TODO

--- cycada/tools/util.py
+++ cycada/tools/util.py
@@ -1,6 +1,7 @@
 import torch 
 from torch.autograd import Variable
-
+# TODO: GET RID OF THIS - Variable is deprecated in Pytorch 
+# Priority - Low
 def make_variable(tensor, volatile=False, requires_grad=True):
     if torch.cuda.is_available():
         tensor = tensor.cuda()
