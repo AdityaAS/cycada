199e4b95899cd2820db2dff12173a577ef5a72dc
Aditya Sarma <aditya@wadhwaniai.org>
2019-06-06 15:45:27 +0530

    dataloader cyclegan changed

--- cycleGAN/train_cycada.sh
+++ cycleGAN/train_cycada.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=0 python -m pdb train.py --name cycada_svhn2mnist_noIdentity \
+python  train.py --name cycada_svhn2mnist_noIdentity \
     --resize_or_crop=None \
     --loadSize=32 --fineSize=32 --which_model_netD n_layers --n_layers_D 3 \
     --model cycle_gan \
     --lambda_A 1 --lambda_B 1 --lambda_identity 0 \
     --no_flip --batchSize 4 \
-    --dataset_mode unaligned --dataroot /efs/data/cyclegan_data/svhn2mnist \
+    --dataset_mode unaligned --dataroot_A /home/users/aditya/data/singleview_opendr_real_ratio --dataroot_B /home/users/aditya/data/singleview_opendr_solid \
     --which_direction BtoA --display_id 1
